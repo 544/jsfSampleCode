384a85463631efeb05ab1829b314902b527266ed
tsukagoshi <tsukagoshi.masato@future.co.jp>
2013-06-14 18:23:24 +0900

    init

---
+++ src/main/java/my/example/jsf/App.java
@@ -0,0 +1,13 @@
+package my.example.jsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
