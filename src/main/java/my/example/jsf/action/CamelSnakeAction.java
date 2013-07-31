03a61169b283c27ea6f2d927c27a00f39a38c3a9
tsukagoshi <tsukagoshi.masato@future.co.jp>
2013-07-31 16:41:41 +0900

    Ajax化した。

--- src/main/java/my/example/jsf/action/CamelSnakeAction.java
+++ src/main/java/my/example/jsf/action/CamelSnakeAction.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("session")
+@Scope("request")
 public class CamelSnakeAction {
 
 	private String input;
